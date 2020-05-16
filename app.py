858890fb2e280ee1ffdd6b9cb638a1490302979d
CraigWhitley <kiadauk@gmail.com>
2020-05-16 04:42:15 +0100

    Added dotenv, initial configuration of mongoengine

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask_mongoengine import MongoEngine
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
 
 
 app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    'host': os.getenv("MONGO_URL")
+}
+
+
+db = MongoEngine(app)
 
 
 @app.route('/')
